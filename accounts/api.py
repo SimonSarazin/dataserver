ad2e6de9f6d50a5055b750f7b924d6299e4785cb
Freddy Limpens <freddy.limpens@gmail.com>
2014-05-31 07:22:41 +0200

    add CMS Editor set of permissions to all created user

--- accounts/api.py
+++ accounts/api.py
@@ -3,6 +3,8 @@ from django.conf.urls import url
 from django.contrib.auth.models import User, Group
 from django.contrib.auth import authenticate, logout
 from django.db import models
+from django.db.models.signals import post_save
+from django.dispatch import receiver
 from tastypie.http import HttpUnauthorized, HttpForbidden
 
 from tastypie import fields
@@ -13,6 +15,9 @@ from tastypie.resources import ModelResource
 from tastypie.utils import trailing_slash
 
 from dataserver.authorization import GuardianAuthorization
+
+from guardian.shortcuts import assign_perm
+
 
 class UserResource(ModelResource):
     class Meta:
@@ -155,3 +160,37 @@ class GroupResource(ModelResource):
     
 # Create API key for every new user
 models.signals.post_save.connect(create_api_key, sender=User)
+
+# Add CMS permissions to every new user 
+@receiver(post_save, sender=User)
+def assign_CMS_editor_perm_to_groups(sender, instance, created, *args, **kwargs):
+    if created:
+        print "instance= %s" % instance
+        assign_perm("cms.view_page", user_or_group=instance)
+        assign_perm("cms.change_page", user_or_group=instance)
+        assign_perm("cms.publish_page", user_or_group=instance)
+        assign_perm("cms.add_title", user_or_group=instance)
+        assign_perm("cms.change_title", user_or_group=instance)
+        assign_perm("cms.delete_title", user_or_group=instance)
+        assign_perm("cms_background_images.add_backgroundimagesplugin", user_or_group=instance)
+        assign_perm("cms_background_images.change_backgroundimagesplugin", user_or_group=instance)
+        assign_perm("cms_background_images.delete_backgroundimagesplugin", user_or_group=instance)
+        assign_perm("cms_carto.add_cartoplugin", user_or_group=instance)
+        assign_perm("cms_carto.change_cartoplugin", user_or_group=instance)
+        assign_perm("cms_carto.delete_cartoplugin", user_or_group=instance)
+        assign_perm("cms_news.add_newsentry", user_or_group=instance)
+        assign_perm("cms_news.change_newsentry", user_or_group=instance)
+        assign_perm("cms_news.delete_newsentry", user_or_group=instance)
+        assign_perm("cms_news.add_newsplugin", user_or_group=instance)
+        assign_perm("cms_news.change_newsplugin", user_or_group=instance)
+        assign_perm("cms_news.delete_newsplugin", user_or_group=instance)
+        assign_perm("djangocms_picture.add_picture", user_or_group=instance)
+        assign_perm("djangocms_picture.change_picture", user_or_group=instance)
+        assign_perm("djangocms_picture.delete_picture", user_or_group=instance)
+        assign_perm("djangocms_text_ckeditor.add_text", user_or_group=instance)
+        assign_perm("djangocms_text_ckeditor.change_text", user_or_group=instance)
+        assign_perm("djangocms_text_ckeditor.delete_text", user_or_group=instance)
+        
+        
+
+
